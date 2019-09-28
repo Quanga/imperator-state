16ceac2af51cff40ee1c9c2a6f4f2a287bf4eb7a
Tim Bewsey <tim@h2s.co.za>
2019-09-28 10:53:08 +0200

    larger comapction

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ class Config {
 								overrideObj.db ||
 								this.getPath("db", process.env.EDGE_DB) ||
 								this.getPath("db", "./edge.db"),
-							compactInterval: 5000
+							compactInterval: 15000
 						}
 					}
 				}
