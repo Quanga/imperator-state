8867f29b07f90c8578f93a181260470a85a6b8a6
Tim Bewsey <tim@h2s.co.za>
2019-10-01 11:17:52 +0200

    updated eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -112,7 +112,7 @@ module.exports = {
 			ref: "origin/stage-1.4",
 			repo: "git@github.com:aecelectronics/Happner3_State.git",
 			path: "/home/admin/state",
-			"post-deploy": "npm install && pm2 reload ecosystem.config.js --env production001 && pm2 save"
+			"post-deploy": "npm install && pm2 reload ecosystem.config.js --env production003 && pm2 save"
 		},
 		production_aws: {
 			user: "ec2-user",
