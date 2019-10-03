b46e1322f3e7f1c7574a0cc9eb3f1ce646dc959c
Tim Bewsey <tim@h2s.co.za>
2019-10-03 11:52:21 +0200

    added ws 7

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -122,7 +122,7 @@ module.exports = {
 		},
 		"production-intelliblast-edge-004": {
 			user: "admin",
-			host: ["nick2"],
+			host: ["192.168.1.14"],
 			ref: "origin/stage-1.4",
 			repo: "git@github.com:aecelectronics/Happner3_State.git",
 			path: "/home/admin/state",
