05f668844da0b38c0fb4ddc16fa3692f2a09d7ba
Tim Bewsey <tim@h2s.co.za>
2019-11-19 12:41:38 +0200

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,27 @@ module.exports = {
 		node: true,
 		browser: true,
 	},
-	extends: "eslint:recommended",
+	extends: ["eslint:recommended", "prettier"],
 	plugins: ["prettier"],
+
 	parserOptions: {
 		ecmaVersion: 2018,
 	},
-	rules: {
-		"prettier/prettier": "error",
-		indent: ["error", "tab"],
-		"linebreak-style": ["error", "unix"],
 
-		semi: ["error", "always"],
+	rules: {
+		"prettier/prettier": ["error"],
+		//indent: ["error", "tab"],
+		//indent: [2, 2, { SwitchCase: 1 }, { MemberExpression: 1 }],
+		//"linebreak-style": ["error", "unix"],
+		//"max-len": 0,
+		//"newline-per-chained-call": [2, { ignoreChainWithDepth: 1 }],
+		//"no-confusing-arrow": 0,
 		"no-console": 0,
-		"max-len": ["error", { code: 100, ignoreStrings: true, tabWidth: 2 }],
+		//"no-unused-vars": 2,
+		//"no-use-before-define": 1,
+
+		//semi: ["error", "always"],
+		// "no-console": 0,
+		//"max-len": ["error", { code: 100, ignoreStrings: true, tabWidth: 2 }],
 	},
 };
